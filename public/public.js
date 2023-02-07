01c14507426c2d09b65a3ceb110254ce514fe844
Aziz Solihin <azizslhn45@gmail.com>
2023-02-07 09:43:32 +0700

    add delete cache

--- public/public.js
+++ public/public.js
@@ -1,33 +1,41 @@
-const publicVapidKey = 'BOjcXb3H8zam6iyaNkBgGNIf2wAY-IjcwaLm93SXCVq_xlHiWGGWDExV_kw6AVd5fNTMx80rkRyAG-c3bSknvyU'
+const publicVapidKey =
+  "BOjcXb3H8zam6iyaNkBgGNIf2wAY-IjcwaLm93SXCVq_xlHiWGGWDExV_kw6AVd5fNTMx80rkRyAG-c3bSknvyU";
 
 // check service workers
-if('serviceWorker' in navigator) send().catch(err => console.error(err))
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) send().catch((err) => console.error(err));
+
+  document.getElementById("clear-cache").addEventListener("click", () => {
+    caches.delete("staticCache");
+    location.reload();
+  });
+});
 
 // register sw, register push, send push
 async function send() {
   // register sw
-  const service = await navigator.serviceWorker.register('sw.js', {
-    scope:'/'
-  })
-  console.log('Service worker registered!', service.scope)
+  const service = await navigator.serviceWorker.register("sw.js", {
+    scope: "/",
+  });
+  console.log("Service worker registered!", service.scope);
 
   // register push
   const subs = await service.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
-  })
-  console.log('Push registered!')
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+  });
+  console.log("Push registered!");
 
   // send push
-  await fetch('/subscribe', {
-    method: 'post',
+  await fetch("/subscribe", {
+    method: "post",
     body: JSON.stringify(subs),
-    headers: {'content-type': 'application/json'}
-  })
+    headers: { "content-type": "application/json" },
+  });
 }
 
 function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4);
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding)
     .replace(/\-/g, "+")
     .replace(/_/g, "/");
